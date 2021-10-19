ad4d62753d39be3fcb1f1cce5a2cdcb672cc4998
Adam Harbisher <aharbisher@gmail.com>
2021-10-19 16:17:04 +0100

    Created Bike class

---
+++ lib/boris_bikes.rb
@@ -0,0 +1,16 @@
+class DockingStation
+    def initialize
+        @bikes = []
+    end
+    def release_bike
+        Bike.new
+    end
+    def dock_bike(bike)
+        @bikes << bike
+    end
+end
+
+class Bike
+    def working?
+    end
+end
--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -1,5 +1,21 @@
-require 'boris_bikes'
+require './lib/boris_bikes'
 
-describe 'DockingStation' do
-    DockingStation = Class.new
+describe DockingStation do
+    it 'an object that describes a docking station' do
+        expect(DockingStation)
+    end
+    it { is_expected.to respond_to(:release_bike) }
+    it { is_expected.to respond_to(:dock_bike).with(1).argument }
+    it 'returns Bike when passsed release_bike method' do
+        @docking_station = DockingStation.new
+        result = @docking_station.send(:release_bike)
+        expect(result).to be_an_instance_of(Bike)
+    end
+end
+
+describe Bike do
+    it 'an object that describes a bike' do
+        expect(Bike)
+    end
+    it { is_expected.to respond_to(:working?)}
 end
